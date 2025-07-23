d0bfaf45b52e61ba5308cc3c26f6b397a8afb5b0
BananaJeanss <18456310+BananaJeanss@users.noreply.github.com>
2025-07-23 14:19:34 +0300

    Refactor command and event handling to use ES modules and improve structure

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
-// Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { config } from 'dotenv';
+import { readdirSync } from "node:fs";
+import { join, dirname } from "node:path";
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { config } from "dotenv";
 
-// env variables
 config();
 const token = process.env.DISCORD_BOT_TOKEN;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+client.commands = new Collection();
+client.cooldowns = new Collection();
+const foldersPath = join(__dirname, "commands");
+const commandFolders = readdirSync(foldersPath);
 
-// Log in to Discord with your client's token
+for (const folder of commandFolders) {
+  const commandsPath = join(foldersPath, folder);
+  const commandFiles = readdirSync(commandsPath).filter((file) =>
+    file.endsWith(".js")
+  );
+  for (const file of commandFiles) {
+    const filePath = join(commandsPath, file);
+    const command = await import(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const eventModule = await import(filePath);
+  const event = eventModule.default || eventModule;
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 client.login(token);
